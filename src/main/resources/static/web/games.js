c1bf8d4c9b2e8243eafe9a44917ed597aba7f39f
ania555 <anna-wojtowicz@wp.pl>
2019-01-18 18:51:21 +0100

    login, logout form

--- src/main/resources/static/web/games.js
+++ src/main/resources/static/web/games.js
@@ -11,6 +11,7 @@ function loadData() {
         var myGames = json.games;
         console.log(myGames);
         showGames(myGames);
+
     })
     .catch((error) => {
         console.log("Request failed: " + error.message)
@@ -27,6 +28,7 @@ function loadLeadBrd() {
        var myPlayers = json;
        console.log(myPlayers);
        showLeaders(myPlayers);
+       listenLogs();
    })
    .catch((error) => {
        console.log("Request failed: " + error.message)
@@ -50,7 +52,20 @@ function showGames(item) {
 
 function showLeaders(item) {
     let container = document.querySelector("#leader-board");
-
+    container.innerHTML = "";
+    let firstRow = document.createElement("tr");
+    container.appendChild(firstRow);
+    let nameH = firstRow.insertCell();
+    let totalH = firstRow.insertCell();
+    let winsH = firstRow.insertCell();
+    let lossesH = firstRow.insertCell();
+    let tiesH = firstRow.insertCell();
+    nameH.innerHTML = "Name";
+    totalH.innerHTML = "Total";
+    winsH.innerHTML = "Won";
+    lossesH.innerHTML = "Lost";
+    tiesH.innerHTML = "Tied";
+    nameH.setAttribute("class", "to_left");
     let sortedItems = item.sort(function(a, b) {
         if (b.totalScore !== a.totalScore) {
             return  b.totalScore - a.totalScore
@@ -59,7 +74,6 @@ function showLeaders(item) {
             return b.win_count - a.win_count
         }
     });
-
     for (let i = 0; i < sortedItems.length; i++) {
     console.log("row")
         let row = document.createElement("tr");
@@ -76,4 +90,111 @@ function showLeaders(item) {
         ties.innerHTML = item[i].tie_count;
         name.setAttribute("class", "to_left");
     }
+}
+
+function listenLogs() {
+    console.log("added");
+    document.querySelector("#login").addEventListener("click", function () {findPlayer()});
+    document.querySelector("#logout").addEventListener("click", function () {logoutPlayer()});
+}
+
+function findPlayer() {
+    console.log("result");
+    let findUser = document.querySelector("#user");
+    let findPassword = document.querySelector("#password");
+    let user = findUser.value;
+    let password = findPassword.value;
+    console.log(user + " " + password);
+    if (user && password) {loginPlayer(user, password)}
+}
+
+
+function loginPlayer(user, password) {
+    fetch("/api/login", {
+    credentials: 'include',
+    headers: {
+    'Content-Type': 'application/x-www-form-urlencoded'
+    },
+    method: 'POST',
+    body: 'userName='+ user + '&password='+ password,
+    })
+    .then(function (data) {
+    console.log('Request success: ', data);
+    console.log("login");
+    loginNow();
+    location.reload();
+    })
+    .catch(function (error) {
+    console.log('Request failure: ', error);
+    });
+}
+
+function logoutPlayer() {
+    console.log("logout");
+
+    fetch("/api/logout", {
+    credentials: 'include',
+    headers: {
+    'Content-Type': 'application/x-www-form-urlencoded'
+    },
+    method: 'POST',
+    })
+    .then(function (data) {
+    console.log('Request success: ', data);
+    location.reload();
+    logoutNow();
+    })
+    .catch(function (error) {
+    console.log('Request failure: ', error);
+    });
+}
+
+function loginNow() {
+    document.getElementById('loginForm').style.visibility = 'hidden';
+    document.getElementById('logoutForm').style.visibility = 'visible';
+    localStorage.setItem('showLogin', 'hidden');
+    localStorage.setItem('showLogout', 'visible');
+}
+
+window.onload = function () {
+        var showIn = localStorage.getItem('showLogin');
+        var showOut = localStorage.getItem('showLogout');
+        if(showIn === 'visible'){
+             document.getElementById('loginForm').style.visibility = 'visible';
+        }
+        else if (showIn === 'hidden'){
+             document.getElementById('loginForm').style.visibility = 'hidden';
+        }
+
+        if (showOut === 'visible'){
+             document.getElementById('logoutForm').style.visibility = 'visible';
+        }
+        else if (showOut === 'hidden') {
+             document.getElementById('logoutForm').style.visibility = 'hidden';
+        }
+}
+
+function logoutNow () {
+    document.getElementById('logoutForm').style.visibility = 'hidden'
+    document.getElementById('loginForm').style.visibility = 'visible'
+    localStorage.setItem('showLogin', 'visible');
+    localStorage.setItem('showLogout', 'hidden');
+}
+
+function signInPlayer() {
+    fetch("/api/players", {
+    credentials: 'include',
+    headers: {
+    'Content-Type': 'application/x-www-form-urlencoded'
+    },
+    method: 'POST',
+    body: 'userName='+ username + '&password='+ password,
+    })
+    .then(function (data) {
+    console.log('Request success: ', data);
+    }).then(function () {
+    })
+    .catch(function (error) {
+    console.log('Request failure: ', error);
+    });
 }
