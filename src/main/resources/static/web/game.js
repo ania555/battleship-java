0c5e2c7e481b023ae4c85571dcd980e096dca859
ania555 <anna-wojtowicz@wp.pl>
2019-01-23 18:49:24 +0100

    placing ships started

--- src/main/resources/static/web/game.js
+++ src/main/resources/static/web/game.js
@@ -259,3 +259,17 @@ function onlySalvoes(item) {
        }
     }
 }
+
+function allowDrop(ev) {
+  ev.preventDefault();
+}
+
+function drag(ev) {
+  ev.dataTransfer.setData("text", ev.target.id);
+}
+
+function drop(ev) {
+  ev.preventDefault();
+  var data = ev.dataTransfer.getData("text");
+  ev.target.appendChild(document.getElementById(data));
+}
